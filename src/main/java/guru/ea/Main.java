fa6d874862cdea6b81332026940c10cc796b7361
Eleonora Artyukhova <125487605+Zhooggas@users.noreply.github.com>
2023-02-16 17:06:10 +0600

    Update Main.java

--- src/main/java/guru/ea/Main.java
+++ src/main/java/guru/ea/Main.java
@@ -2,6 +2,6 @@ package guru.ea;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!!");
     }
 }
